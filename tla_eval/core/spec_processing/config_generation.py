8d65ba3106789670ab764f7239a7dbe7443d35ca
Qian-Cheng-nju <Qian-Cheng-nju@users.noreply.github.com>
2026-04-28 10:56:11 +0000

    refactor: drop dead tla_eval/core/spec_processing package

--- tla_eval/core/spec_processing/config_generation.py
+++
@@ -1,9 +0,0 @@
-"""
-Configuration Generation Module
-
-This module handles LLM-based generation of YAML configurations from TLA+ specifications.
-"""
-
-from .spec_converter import generate_config_from_tla
-
-__all__ = ['generate_config_from_tla']
